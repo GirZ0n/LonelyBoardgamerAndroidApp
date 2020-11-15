5e4edf91765d3dbade83f3b707b8f1f03fc87cb2
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-11-15 20:20:30 +0300

    Added encryptedSharedPreferences for tokens

---
+++ app/src/main/java/com/twoIlya/android/lonelyboardgamer/sharedPref/TokenPref.kt
@@ -0,0 +1,76 @@
+package com.twoIlya.android.lonelyboardgamer.sharedPref
+
+import android.content.Context
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+
+object TokenPref {
+
+    private const val SHARED_PREF_NAME = "token_preferences"
+    private const val SERVER_TOKEN_KEY = "server_token"
+    private const val VK_TOKEN_KEY = "vk_token"
+
+    fun getServerToken(context: Context): String {
+        val masterKey = MasterKey.Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        val sharedPreferences = EncryptedSharedPreferences.create(
+            context,
+            SHARED_PREF_NAME,
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
+        return sharedPreferences.getString(SERVER_TOKEN_KEY, "") ?: ""
+    }
+
+    fun setServerToken(context: Context, value: String) {
+        val masterKey = MasterKey.Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        val sharedPreferences = EncryptedSharedPreferences.create(
+            context,
+            SHARED_PREF_NAME,
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
+        sharedPreferences.edit().putString(SERVER_TOKEN_KEY, value).apply()
+    }
+
+    fun getVKToken(context: Context): String {
+        val masterKey = MasterKey.Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        val sharedPreferences = EncryptedSharedPreferences.create(
+            context,
+            SHARED_PREF_NAME,
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
+        return sharedPreferences.getString(VK_TOKEN_KEY, "") ?: ""
+    }
+
+    fun setVKToken(context: Context, value: String) {
+        val masterKey = MasterKey.Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        val sharedPreferences = EncryptedSharedPreferences.create(
+            context,
+            SHARED_PREF_NAME,
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
+        sharedPreferences.edit().putString(VK_TOKEN_KEY, value).apply()
+    }
+}
