c2df1be66357766dd6238d60893108fc5af7e0ae
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-11-17 21:33:41 +0300

    Small MVVM improvements

--- app/src/main/java/com/twoIlya/android/lonelyboardgamer/activities/login/LoginViewModel.kt
+++ app/src/main/java/com/twoIlya/android/lonelyboardgamer/activities/login/LoginViewModel.kt
@@ -1,28 +1,24 @@
 package com.twoIlya.android.lonelyboardgamer.activities.login
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
-import com.twoIlya.android.lonelyboardgamer.api.ServerResponse
-import com.twoIlya.android.lonelyboardgamer.repository.Repository
-import com.twoIlya.android.lonelyboardgamer.sharedPref.TokenPref
+import androidx.lifecycle.ViewModel
+import com.twoIlya.android.lonelyboardgamer.repository.ServerRepository
+import com.twoIlya.android.lonelyboardgamer.repository.CacheRepository
+import com.twoIlya.android.lonelyboardgamer.repository.TokenRepository
 
-class LoginViewModel(private val app: Application): AndroidViewModel(app) {
-    private val repo = Repository
+class LoginViewModel : ViewModel() {
+    private val repo = ServerRepository
 
-    val loginServerResponse: LiveData<ServerResponse>
     private val token = MutableLiveData<String>()
-
-    init {
-        loginServerResponse = Transformations.switchMap(token) { token ->
-            repo.login(token)
-        }
+    val loginServerResponse = Transformations.switchMap(token) { token ->
+        repo.login(token)
     }
 
     fun login(accessToken: String) {
         token.value = accessToken
-        TokenPref.setVKToken(app, accessToken)
+        TokenRepository.setVKToken(accessToken)
     }
+
+    fun isUserLoggedIn() = CacheRepository.isLoggedIn()
 }
