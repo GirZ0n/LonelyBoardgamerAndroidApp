83684ca86f3c8ceaefb60eae543a8abee5d21451
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-11-19 20:12:48 +0300

    Added ErrorActivity

---
+++ app/src/main/java/com/twoIlya/android/lonelyboardgamer/activities/error/ErrorActivity.kt
@@ -0,0 +1,12 @@
+package com.twoIlya.android.lonelyboardgamer.activities.error
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.twoIlya.android.lonelyboardgamer.R
+
+class ErrorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_error)
+    }
+}
