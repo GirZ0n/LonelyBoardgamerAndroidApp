3c510edffe45cc8ce04a31bd02f3bda1c7a9e502
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-11-19 20:13:45 +0300

    Added Token data class

---
+++ app/src/main/java/com/twoIlya/android/lonelyboardgamer/dataClasses/Token.kt
@@ -0,0 +1,5 @@
+package com.twoIlya.android.lonelyboardgamer.dataClasses
+
+import com.twoIlya.android.lonelyboardgamer.repository.ServerRepositoryResponse
+
+data class Token(val value: String) : ServerRepositoryResponse()
