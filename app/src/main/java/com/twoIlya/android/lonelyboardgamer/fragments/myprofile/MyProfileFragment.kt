9b2745a5f38280552df77922bc013bc75b606950
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-11-26 09:20:35 +0300

    Added logic to MyProfileFragment

--- app/src/main/java/com/twoIlya/android/lonelyboardgamer/fragments/myprofile/MyProfileFragment.kt
+++ app/src/main/java/com/twoIlya/android/lonelyboardgamer/fragments/myprofile/MyProfileFragment.kt
@@ -1,22 +1,76 @@
 package com.twoIlya.android.lonelyboardgamer.fragments.myprofile
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.twoIlya.android.lonelyboardgamer.R
+import com.twoIlya.android.lonelyboardgamer.activities.error.ErrorActivity
+import com.twoIlya.android.lonelyboardgamer.activities.login.LoginActivity
+import com.twoIlya.android.lonelyboardgamer.dataClasses.EventType
+import com.twoIlya.android.lonelyboardgamer.databinding.FragmentMyProfileBinding
 
 class MyProfileFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var binding: FragmentMyProfileBinding
+    private val viewModel: MyProfileViewModel by lazy {
+        ViewModelProvider(this).get(
+            MyProfileViewModel::class.java
+        )
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_my_profile, container, false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        viewModel.events.observe(viewLifecycleOwner) {
+            Log.d(TAG, "Event: $it")
+
+            when (it.isHandle) {
+                true -> return@observe
+                false -> it.isHandle = true
+            }
+
+            when (it.type) {
+                EventType.Warning -> {
+                    Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
+                }
+                EventType.Error -> {
+                    val intent = ErrorActivity.newActivity(requireContext(), it.message)
+                    startActivity(intent)
+                    activity?.finish()
+                }
+                EventType.Move -> {
+                    val intent = when (it.message) {
+                        "Login" -> LoginActivity.newActivity(requireContext())
+                        else -> ErrorActivity.newActivity(requireContext(), "Unknown destination")
+                    }
+                    startActivity(intent)
+                    activity?.finish()
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "MyProfileFragment_TAG"
     }
 }
