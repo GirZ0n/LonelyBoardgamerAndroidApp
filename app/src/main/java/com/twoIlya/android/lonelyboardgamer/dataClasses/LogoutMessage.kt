e1bcf390ed55babfbc24a258eaa637c806dfef6a
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-11-26 20:38:15 +0300

    Added logoutMessage

---
+++ app/src/main/java/com/twoIlya/android/lonelyboardgamer/dataClasses/LogoutMessage.kt
@@ -0,0 +1,5 @@
+package com.twoIlya.android.lonelyboardgamer.dataClasses
+
+import com.twoIlya.android.lonelyboardgamer.repository.ServerRepositoryResponse
+
+data class LogoutMessage(val value: String): ServerRepositoryResponse()
