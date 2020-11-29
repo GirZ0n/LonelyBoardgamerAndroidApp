719c9b984ccf8496f050669f526e6fa957badae3
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-11-29 12:45:37 +0300

    Added adapter for button drawable

--- app/src/main/java/com/twoIlya/android/lonelyboardgamer/BindingUtils.kt
+++ app/src/main/java/com/twoIlya/android/lonelyboardgamer/BindingUtils.kt
@@ -1,9 +1,11 @@
 package com.twoIlya.android.lonelyboardgamer
 
+import android.widget.Button
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.databinding.BindingConversion
 import androidx.lifecycle.LiveData
+import com.github.ybq.android.spinkit.style.ThreeBounce
 import com.squareup.picasso.Picasso
 
 
@@ -17,4 +19,23 @@ fun loadImage(view: ImageView, imageUrl: String?) {
         .load(imageUrl)
         .placeholder(R.drawable.ic_user_placeholder)
         .into(view)
+}
+
+@BindingAdapter("bind:isLoading")
+fun updateDrawable(view: Button, isLoading: Boolean) {
+    when (isLoading) {
+        true -> {
+            val dots = ThreeBounce()
+            dots.setBounds(0, 0, 100, 100)
+            dots.start()
+            view.setCompoundDrawables(dots, null, null, null)
+        }
+        false -> {
+            val leftDrawable = view.compoundDrawables.first()
+            if (leftDrawable is ThreeBounce) {
+                leftDrawable.stop()
+            }
+            view.setCompoundDrawables(null, null, null, null)
+        }
+    }
 }
