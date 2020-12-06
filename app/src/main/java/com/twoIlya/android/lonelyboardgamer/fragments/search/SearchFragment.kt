593318e570c3c76a0a5ee3e583d3c34fc9b91882
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-12-06 16:21:09 +0300

    Added recycler view

--- app/src/main/java/com/twoIlya/android/lonelyboardgamer/fragments/search/SearchFragment.kt
+++ app/src/main/java/com/twoIlya/android/lonelyboardgamer/fragments/search/SearchFragment.kt
@@ -4,15 +4,104 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
+import com.twoIlya.android.lonelyboardgamer.OnItemClickListener
 import com.twoIlya.android.lonelyboardgamer.R
+import com.twoIlya.android.lonelyboardgamer.databinding.FragmentSearchBinding
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.distinctUntilChangedBy
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.launch
 
 class SearchFragment : Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
+    private val viewModel: SearchViewModel by lazy { ViewModelProvider(this).get(SearchViewModel::class.java) }
+    private lateinit var adapter: SearchAdapter
+
+    private var searchJob: Job? = null
+
+    private fun search() {
+        searchJob?.cancel()
+        searchJob = lifecycleScope.launch {
+            viewModel.search().collectLatest {
+                adapter.submitData(it)
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_search, container, false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initAdapter()
+        search()
+        // initSearch()
+        binding.retryButton.setOnClickListener { adapter.retry() }
+    }
+
+    private fun initAdapter() {
+        adapter = SearchAdapter(object : OnItemClickListener {
+            override fun onItemClicked(id: Int) {
+                Toast.makeText(requireContext(), id.toString(), Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        binding.list.adapter = adapter.withLoadStateHeaderAndFooter(
+            header = SearchLoadStateAdapter { adapter.retry() },
+            footer = SearchLoadStateAdapter { adapter.retry() }
+        )
+
+        adapter.addLoadStateListener { loadState ->
+            // Only show the list if refresh succeeds.
+            binding.list.isVisible = loadState.source.refresh is LoadState.NotLoading
+            // Show loading spinner during initial load or refresh.
+            binding.progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+            // Show the retry state if initial load or refresh fails.
+            binding.retryButton.isVisible = loadState.source.refresh is LoadState.Error
+
+            // Toast on any error, regardless of whether it came from RemoteMediator or PagingSource
+            val errorState = loadState.source.append as? LoadState.Error
+                ?: loadState.source.prepend as? LoadState.Error
+                ?: loadState.append as? LoadState.Error
+                ?: loadState.prepend as? LoadState.Error
+            errorState?.let {
+                Toast.makeText(
+                    requireContext(),
+                    "\uD83D\uDE28 Wooops ${it.error}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    private fun initSearch() {
+        // Scroll to top when the list is refreshed from network.
+        lifecycleScope.launch {
+            adapter.loadStateFlow
+                // Only emit when REFRESH LoadState for RemoteMediator changes.
+                .distinctUntilChangedBy { it.refresh }
+                // Only react to cases where Remote REFRESH completes i.e., NotLoading.
+                .filter { it.refresh is LoadState.NotLoading }
+                .collect { binding.list.scrollToPosition(0) }
+        }
     }
 }
