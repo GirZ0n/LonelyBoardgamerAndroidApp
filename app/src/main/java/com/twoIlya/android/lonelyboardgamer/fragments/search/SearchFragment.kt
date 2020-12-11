c1ebe788cd23e54bd34c3440c4393625f60f8b29
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-12-11 18:05:36 +0300

    Architectural changes: 1) Added error handler; 2) Moved load state listener to viewmodel.

--- app/src/main/java/com/twoIlya/android/lonelyboardgamer/fragments/search/SearchFragment.kt
+++ app/src/main/java/com/twoIlya/android/lonelyboardgamer/fragments/search/SearchFragment.kt
@@ -1,25 +1,22 @@
 package com.twoIlya.android.lonelyboardgamer.fragments.search
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import androidx.paging.LoadState
 import com.twoIlya.android.lonelyboardgamer.OnItemClickListener
 import com.twoIlya.android.lonelyboardgamer.R
+import com.twoIlya.android.lonelyboardgamer.activities.error.ErrorActivity
+import com.twoIlya.android.lonelyboardgamer.activities.login.LoginActivity
+import com.twoIlya.android.lonelyboardgamer.dataClasses.EventType
 import com.twoIlya.android.lonelyboardgamer.databinding.FragmentSearchBinding
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.distinctUntilChangedBy
-import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
 
 class SearchFragment : Fragment() {
@@ -27,17 +24,6 @@ class SearchFragment : Fragment() {
     private lateinit var binding: FragmentSearchBinding
     private val viewModel: SearchViewModel by lazy { ViewModelProvider(this).get(SearchViewModel::class.java) }
     private lateinit var adapter: SearchAdapter
-
-    private var searchJob: Job? = null
-
-    private fun search() {
-        searchJob?.cancel()
-        searchJob = lifecycleScope.launch {
-            viewModel.search().collectLatest {
-                adapter.submitData(it)
-            }
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -54,9 +40,39 @@ class SearchFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initAdapter()
-        search()
-        // initSearch()
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
         binding.retryButton.setOnClickListener { adapter.retry() }
+
+        viewModel.events.observe(viewLifecycleOwner) {
+            Log.d(TAG, "Event: $it")
+
+            when (it.isHandle) {
+                true -> return@observe
+                false -> it.isHandle = true
+            }
+
+            when (it.type) {
+                EventType.Notification -> {
+                    Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
+                }
+                EventType.Error -> {
+                    val intent = ErrorActivity.newActivity(requireContext(), it.message)
+                    startActivity(intent)
+                    activity?.finish()
+                }
+                EventType.Move -> {
+                    val intent = when (it.message) {
+                        "Login" -> LoginActivity.newActivity(requireContext())
+                        else -> ErrorActivity.newActivity(requireContext(), "Unknown destination")
+                    }
+                    startActivity(intent)
+                    activity?.finish()
+                }
+            }
+        }
     }
 
     private fun initAdapter() {
@@ -64,7 +80,10 @@ class SearchFragment : Fragment() {
             override fun onItemClicked(id: Int) {
                 val bundle = Bundle()
                 bundle.putInt("id", id)
-                findNavController().navigate(R.id.action_searchFragment_to_userProfileFragment, bundle)
+                findNavController().navigate(
+                    R.id.action_searchFragment_to_userProfileFragment,
+                    bundle
+                )
             }
         })
 
@@ -73,38 +92,16 @@ class SearchFragment : Fragment() {
             footer = SearchLoadStateAdapter { adapter.retry() }
         )
 
-        adapter.addLoadStateListener { loadState ->
-            // Only show the list if refresh succeeds.
-            binding.list.isVisible = loadState.source.refresh is LoadState.NotLoading
-            // Show loading spinner during initial load or refresh.
-            binding.progressBar.isVisible = loadState.source.refresh is LoadState.Loading
-            // Show the retry state if initial load or refresh fails.
-            binding.retryButton.isVisible = loadState.source.refresh is LoadState.Error
+        adapter.addLoadStateListener(viewModel::loadStateListener)
 
-            // Toast on any error, regardless of whether it came from RemoteMediator or PagingSource
-            val errorState = loadState.source.append as? LoadState.Error
-                ?: loadState.source.prepend as? LoadState.Error
-                ?: loadState.append as? LoadState.Error
-                ?: loadState.prepend as? LoadState.Error
-            errorState?.let {
-                Toast.makeText(
-                    requireContext(),
-                    "\uD83D\uDE28 Wooops ${it.error}",
-                    Toast.LENGTH_LONG
-                ).show()
+        viewModel.searchLiveData.observe(viewLifecycleOwner) {
+            lifecycleScope.launch {
+                adapter.submitData(it)
             }
         }
     }
 
-    private fun initSearch() {
-        // Scroll to top when the list is refreshed from network.
-        lifecycleScope.launch {
-            adapter.loadStateFlow
-                // Only emit when REFRESH LoadState for RemoteMediator changes.
-                .distinctUntilChangedBy { it.refresh }
-                // Only react to cases where Remote REFRESH completes i.e., NotLoading.
-                .filter { it.refresh is LoadState.NotLoading }
-                .collect { binding.list.scrollToPosition(0) }
-        }
+    companion object {
+        private const val TAG = "SearchFragment_TAG"
     }
 }
